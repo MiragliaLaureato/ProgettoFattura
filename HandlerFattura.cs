f68855921d0a7462b0380e3ffd5d031f57be1d08
MiragliaLaureato <53180702+MiragliaLaureato@users.noreply.github.com>
2021-10-15 11:42:42 +0200

    Add files via upload

--- HandlerFattura.cs
+++ HandlerFattura.cs
@@ -19,23 +19,48 @@ namespace Progetto_Fatture
 
         public void CreaFattura(Intestazione i, Dettagli d)
         {
-            Fattura tmp = new Fattura {
-            Dettagli = new List<Dettagli> { d },
-            Header = i
+            Fattura tmp = new Fattura
+            {
+                Dettagli = new List<Dettagli> { d },
+                Header = i
             };
             Aggiungifattura(tmp);
         }
-         
+
         public void Aggiungifattura(Fattura f)
         {
             ListaFatture.Add(f);
         }
 
 
-        public List<Fattura> CercaFatturaByNomeCliente(string nome) 
+        public List<Fattura> CercaFatturaByNomeCliente(string nome)
             => ListaFatture
             .Where(
                 f => f.Header.Cliente.RagSoc.Equals(nome)
                 ).ToList();
+
+
+        public List<Fattura> CercaFatturaByProdotto(string prodotto)
+        {
+
+            List<Fattura> result = new List<Fattura>();
+
+            foreach (var fat in ListaFatture)
+            {
+                Prodotto prod = null;
+                foreach (var det in fat.Dettagli)
+                {
+                    prod = det.Prodotti.Where(x => x.Nome.Equals(prodotto)).FirstOrDefault();
+                    if (prod != null)
+                        break;
+
+                }
+                if (prod != null)
+                    result.Add(fat);
+            }
+
+            return result;
+
+        }
     }
 }
